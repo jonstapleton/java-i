6a747f7042f25213f3f20d547b51ab7875d07611
octojon <mail@octojon.dev>
2026-02-10 12:25:50 -0500

    added array examples

--- arrays-examples/terminal/Main.java
+++ arrays-examples/terminal/Main.java
@@ -2,8 +2,29 @@ public class Main {
     public static void main(String[] args) {
         Console c = new Console();
         
-        for(int i=0;i<100;i++) {
-            c.println(i);
+        // arrays for translator
+        String[] englishWords = {
+            "red",
+            "blue",
+            "green",
+            "yellow"
+        };
+
+        String[] spanishWords ={
+            "rojo",
+            "azul",
+            "verde",
+            "amarillo"
+        };
+
+        c.println("Type a color in english");
+        String a = c.readLine("");
+        int address = -1;
+        for(int i=0;i<englishWords.length;i++) {
+            if(a.equals(englishWords[i])) {
+                address = i;
+            }
         }
+        c.println(spanishWords[address])
     }
 }
